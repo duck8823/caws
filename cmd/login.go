518edd62458d1bb47d384aaaba6fd04a13e842be
shunsuke maeda <duck8823@gmail.com>
2019-11-03 16:57:07 +0900

    Update command and descriptions

--- cmd/login.go
+++ cmd/login.go
@@ -30,13 +30,11 @@ import (
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "login AWS with STS",
+	Long: `login AWS with AWS Security Token Service (STS)
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This command set credentials to environment variables.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("login called")
 	},
