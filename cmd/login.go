17a6e71f90b5dafb2f9b7b713f41b3694f974a19
shunsuke maeda <duck8823@gmail.com>
2019-11-03 18:32:33 +0900

    Implements login command

--- cmd/login.go
+++ cmd/login.go
@@ -23,8 +23,11 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // loginCmd represents the login command
@@ -35,21 +38,55 @@ var loginCmd = &cobra.Command{
 
 This command set credentials to environment variables.
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("login called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		prof, err := cmd.Flags().GetString("profile")
+		if err != nil {
+			return fmt.Errorf("Failed to parse profile '%s': %+v", prof, err)
+		}
+
+		sess, err := session.NewSessionWithOptions(session.Options{Profile: prof})
+		if err != nil {
+			return fmt.Errorf("Failed to start new session: %+v", err)
+		}
+
+		arn, err := cmd.Flags().GetString("arn-mfa")
+		if err != nil {
+			return fmt.Errorf("Failed to parse arn: %+v", arn, err)
+		}
+
+		code, err := stscreds.StdinTokenProvider()
+		if err != nil {
+			return fmt.Errorf("Failed to get token code: %+v", err)
+		}
+
+		svc := sts.New(sess)
+		input := &sts.GetSessionTokenInput{
+			SerialNumber: &arn,
+			TokenCode:    &code,
+		}
+
+		result, err := svc.GetSessionToken(input)
+		if err != nil {
+			return fmt.Errorf("Failed to get session token: %+v", err)
+		}
+
+		if err := os.Setenv("AWS_ACCESS_KEY_ID", *result.Credentials.AccessKeyId); err != nil {
+			return fmt.Errorf("Failed to set environment variable: %+v", err)
+		}
+		if err := os.Setenv("AWS_SECRET_ACCESS_KEY", *result.Credentials.SecretAccessKey); err != nil {
+			return fmt.Errorf("Failed to set environment variable: %+v", err)
+		}
+		if err := os.Setenv("AWS_SESSION_TOKEN", *result.Credentials.SessionToken); err != nil {
+			return fmt.Errorf("Failed to set environment variable: %+v", err)
+		}
+
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(loginCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	loginCmd.Flags().StringP("arn-mfa", "m", "myRoleArn", "An arn of the MFA device")
+	loginCmd.Flags().StringP("profile", "p", "default", "A name of profile use to get session token")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/duck8823/caws
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.25.25 // indirect
+	github.com/aws/aws-sdk-go v1.25.25
 	github.com/spf13/cobra v0.0.5
 )
